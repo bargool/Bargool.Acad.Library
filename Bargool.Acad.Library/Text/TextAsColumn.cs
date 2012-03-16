2bfa1ed9efa6556cffb1e70d9abb5368db2ef4c6
bargool <bargool@gmail.com>
2012-03-16 11:39:36 +0400

    Initial commit

---
+++ Bargool.Acad.Library/Text/TextAsColumn.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * User: aleksey.nakoryakov
+ * Date: 15.03.12
+ * Time: 16:19
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+
+namespace Bargool.Acad.Library.Text
+{
+	/// <summary>
+	/// Класс для работы со столбцом, сформированным из отдельных текстов
+	/// </summary>
+	public class TextAsColumn<T> where T : DBText
+	{
+		public List<T> cells { get; private set; }
+		public TextAsColumn()
+		{
+			cells = new List<T>();
+		}
+		
+		public TextAsColumn(T header, T[] texts)
+		{
+			cells = new List<T>();
+			cells.Add(header);
+			cells.AddRange(FindCol(texts));
+		}
+		
+		List<T> FindCol(T[] texts)
+		{
+			Point3d min = cells.First().GeometricExtents.MinPoint;
+			Point3d max = cells.First().GeometricExtents.MaxPoint;
+			return texts
+				.Where(n =>{
+				       	Point3d nmin = n.GeometricExtents.MinPoint;
+				       	Point3d nmax = n.GeometricExtents.MaxPoint;
+				       	if (((min.X>=nmin.X)&&(min.X<=nmax.X))||
+				       	    ((max.X>=nmin.X)&&(max.X<=nmax.X))||
+				       	    ((min.X<=nmin.X)&&(max.X>=nmax.X)))
+				       	{
+				       		if (min.Y>nmax.Y)
+				       			return true;
+				       	}
+				       	return false;
+				       })
+				.Select(n => n)
+				.OrderByDescending(n => n.GeometricExtents.MinPoint.Y)
+				.ToList();
+		}
+	}
+}
---
+++ Bargool.Acad.Library/Text/TextAsTable.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * User: aleksey.nakoryakov
+ * Date: 15.03.12
+ * Time: 15:59
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace Bargool.Acad.Library.Text
+{
+	/// <summary>
+	/// Класс для работы с набором столбцов, сформированных из отдельных текстов
+	/// </summary>
+	public class TextAsTable<T> where T: DBText
+	{
+		public List<TextAsColumn<T>>  columns { get; private set; }
+		
+//		public List<string> Titles{
+//			get {
+//				return cols
+//					.Select(n => n.cells.First().TextString)
+//					.ToList();
+//			}
+//		}
+		
+		public TextAsTable()
+		{
+			columns = new List<TextAsColumn<T>>();
+		}
+		
+		public TextAsTable(T[] textEntities, Func<T, bool> IsHeader)
+			: this(textEntities, IsHeader, (n => true)) {}
+		
+		public TextAsTable(T[] textEntities, Func<T, bool> IsHeader, Func<T, bool> IsColText)
+			: this()
+		{
+			IEnumerable<T> texts = textEntities
+				.Where(n => IsColText(n));
+			foreach (T txt in texts
+			         .Where(n => IsHeader(n)))
+			{
+				columns.Add(new TextAsColumn<T>(txt, texts.ToArray()));
+			}
+		}
+		
+//		public void ChangeTableValues(Dictionary<string, string[]> Table)
+//		{
+//			foreach (KeyValuePair<string, string[]> kvp in Table)
+//			{
+//				acad.DocumentManager.MdiActiveDocument.Editor.WriteMessage("\nМеняем {0}", kvp.Key);
+//				cols.Find(n=>n.cells.First().TextString==kvp.Key).ChangeCellsValues(kvp.Value);
+//			}
+//		}
+		
+//		public override string ToString()
+//		{
+//			StringBuilder sb = new StringBuilder();
+//			foreach (SLTableColumn row in cols)
+//				sb.AppendLine(row.ToString());
+//			return sb.ToString();
+//		}
+	}
+}
