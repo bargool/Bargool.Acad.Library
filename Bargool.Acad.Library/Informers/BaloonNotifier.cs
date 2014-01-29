58960f7fcbddf7c8cb2a0bcdaf401c1981dad887
Alexey Nakoryakov <alexey.nakoryakov@gms.ru>
2014-01-29 11:28:12 +0400

    Начал делать служебный класс нотификации с помощью baloon window

---
+++ Bargool.Acad.Library/Informers/BaloonNotifier.cs
@@ -0,0 +1,90 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: alexey.nakoryakov
+ * Date: 29.01.2014
+ * Time: 11:22
+ */
+using System;
+using System.Text;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Windows;
+
+namespace Bargool.Acad.Library.Informers
+{
+	/// <summary>
+	/// Description of BaloonNotifier.
+	/// </summary>
+	public class BaloonNotifier : IInformer
+	{
+		StringBuilder sb;
+		
+		EventHandler notifyHandler = null;
+		
+		public BaloonNotifier()
+		{
+			sb = new StringBuilder();
+		}
+		
+		public void Notify(string message)
+		{
+			sb.AppendLine(message);
+			if (notifyHandler == null)
+			{
+				notifyHandler = new EventHandler(Application_Idle);
+				Application.Idle += notifyHandler;
+			}
+		}
+
+		void Application_Idle(object sender, EventArgs e)
+		{
+			if (sb.Length != 0)
+			{
+				Document doc = Application.DocumentManager.MdiActiveDocument;
+				TrayItem ti = new TrayItem();
+				ti.ToolTipText = "Йо!";
+				ti.Icon = doc.GetStatusBar().TrayItems[0].Icon;
+				Application.StatusBar.TrayItems.Add(ti);
+				Application.StatusBar.Update();
+				ti.CloseBubbleWindows();
+				TrayItemBubbleWindow bw = new TrayItemBubbleWindow();
+				bw.Title = "Хей хо!";
+				bw.Text = sb.ToString();
+				bw.IconType = IconType.Information;
+				ti.ShowBubbleWindow(bw);
+
+				bw.Closed += delegate { CloseBw(ti); };
+			}
+			
+
+			Application.Idle -= notifyHandler;
+			sb.Clear();
+			notifyHandler = null;
+		}
+		
+		void CloseBw(TrayItem ti)
+		{
+			try
+			{
+				Application.StatusBar.TrayItems.Remove(ti);
+				Application.StatusBar.Update();
+			}
+			catch
+			{ }
+		}
+
+		public void Alert(string message)
+		{
+			throw new NotImplementedException();
+		}
+		
+		public void Warning(string message)
+		{
+			throw new NotImplementedException();
+		}
+		
+		public void Log(string message)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Bargool.Acad.Library/Informers/IInformer.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: alexey.nakoryakov
+ * Date: 29.01.2014
+ * Time: 10:09
+ */
+using System;
+
+namespace Bargool.Acad.Library.Informers
+{
+	/// <summary>
+	/// Description of IInformer.
+	/// </summary>
+	public interface IInformer
+	{
+		void Notify(string message);
+		void Alert(string message);
+		void Warning(string message);
+		void Log(string message);
+	}
+}
